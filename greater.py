bc6b475e71834cd55eef19c990ff7e4e1d43aef4
modi295 <59691163+modi295@users.noreply.github.com>
2020-01-29 16:02:00 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,10 @@
+x = int(input(" enter value of x "))
+y = int(input(" enter value of y "))
+z = int(input(" enter value of z "))
+if x>y and x>z:
+    print(" x is greater   {}".format(x))
+elif y>x and y>z:
+    print(" y is greater  {}".format(y))
+else:
+    print(" z is greater   {}".format(z))
+print(" ...over...")        
