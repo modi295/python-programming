8477b5b3e27b599a927a4cb4111fcdfe3ffbadd3
modi295 <59691163+modi295@users.noreply.github.com>
2020-01-29 16:14:33 +0530

    Create swapping3.py

---
+++ swapping3.py
@@ -0,0 +1,10 @@
+x = int(input("enter value of x "))
+y = int(input("enter value of y "))
+print("value of x is = {}".format(x))
+print("value of y is = {}".format(y))
+x = x + y
+y = x - y
+x = x - y
+print("...value of x and y after swapping... ")
+print("value of x is = {}".format(x))
+print("value of y is = {}".format(y))
