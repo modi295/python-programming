677f0b5d4309803fd90592b2142b1350626e283d
modi295 <59691163+modi295@users.noreply.github.com>
2020-01-29 16:15:18 +0530

    Create swapping4.py

---
+++ swapping4.py
@@ -0,0 +1,10 @@
+x = int(input("enter value of x "))
+y = int(input("enter value of y "))
+print("value of x is = {}".format(x))
+print("value of y is = {}".format(y))
+temp =  x
+x = y
+y = temp
+print("...value of x and y after swapping... ")
+print("value of x is = {}".format(x))
+print("value of y is = {}".format(y))
